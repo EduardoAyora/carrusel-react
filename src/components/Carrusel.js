d5ed16d7d50a1cc450b126b2200a201808201a2e
EduardoAyora <eduardoaayora64@gmail.com>
2022-11-07 16:16:39 -0500

    Implementacion de logica

--- src/components/Carrusel.js
+++ src/components/Carrusel.js
@@ -38,11 +38,37 @@ export default class Carrusel extends Component {
     return (
       <div className='container'>
         <button className='button left' onClick={this.clicIzquierda}>
-          iz
+          <svg
+            style={{ width: '2rem' }}
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth='1.5'
+            stroke='currentColor'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M15.75 19.5L8.25 12l7.5-7.5'
+            />
+          </svg>
         </button>
         {this.props.childComponents[this.state.indiceDeComponenteMostrado]}
         <button className='button right' onClick={this.clicDerecha}>
-          der
+          <svg
+            style={{ width: '2rem' }}
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth='1.5'
+            stroke='currentColor'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M8.25 4.5l7.5 7.5-7.5 7.5'
+            />
+          </svg>
         </button>
       </div>
     )
