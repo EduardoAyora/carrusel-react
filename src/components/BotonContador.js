2081760654f0f5ac375d6136ab1f4b04d8761bcc
EduardoAyora <eduardoaayora64@gmail.com>
2022-11-09 21:55:21 -0500

    Boton contador

--- src/components/BotonContador.js
+++ src/components/BotonContador.js
@@ -1,28 +1,49 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 
-export default class BotonContador extends Component {
-  state = {
-    contador: 0,
-  }
+// export default class BotonContador extends Component {
+//   state = {
+//     contador: 0,
+//   }
 
-  componentDidUpdate(_, prevState) {
-    if (prevState.contador !== this.state.contador) {
-      alert(this.state.contador)
+//   componentDidUpdate(_, prevState) {
+//     if (prevState.contador !== this.state.contador) {
+//       alert(this.state.contador)
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <button
+//         className='btn-agregar'
+//         onClick={() => {
+//           this.setState({
+//             contador: this.state.contador + 1,
+//           })
+//         }}
+//       >
+//         Aumentar Contador
+//       </button>
+//     )
+//   }
+// }
+
+export default function BotonContador() {
+  const [contador, setContador] = useState(0)
+
+  useEffect(() => {
+    if (contador != 0) {
+      alert(contador)
     }
-  }
+  }, [contador])
 
-  render() {
-    return (
-      <button
-        className='btn-agregar'
-        onClick={() => {
-          this.setState({
-            contador: this.state.contador + 1,
-          })
-        }}
-      >
-        Aumentar Contador
-      </button>
-    )
-  }
+  return (
+    <button
+      className='btn-agregar'
+      onClick={() => {
+        setContador(contador + 1)
+      }}
+    >
+      Aumentar Contador
+    </button>
+  )
 }
